c665259e3c35f09eddd1616c6554a6d8127e3c69
mash-97 <mahimnhd97@gmail.com>
2023-05-20 01:19:50 +0600

    dis

--- mash-world/tests/mash-world/mash-world.js
+++ mash-world/tests/mash-world/mash-world.js
@@ -2,185 +2,7 @@ function getRndInteger(min, max) {
   return Math.floor(Math.random() * (max - min + 1) ) + min;
 }
 
-/* Board Matrix Size */
-N = 700;
-M = 700;
-let BOARD = document.getElementById("board");
-BOARD.style.width = N+"px";
-BOARD.style.height = M+"px";
 
-
-let c1 = document.getElementById("c1");
-let c2 = document.getElementById("c2");
-let c3 = document.getElementById("c3");
-
-
-function movc(c, ts, ls, tl, ll){
-  let cv_t = parseInt(c.style.top) || 0;
-  let cv_l = parseInt(c.style.left) || 0;
-  
-  if(tl>=0 && cv_t>=tl){
-    c.style.top = "0px";
-  }
-  else{
-    c.style.top = (cv_t+ts)+"px";
-  }
-  if(ll>=0 && cv_l>=ll){
-    c.style.left = "0px";
-  }
-  else{
-    c.style.left = (cv_l+ls)+"px";
-  }
-}
-
-let y1, y2, y3;
-function start() {
-  y1 = setInterval(()=>movc(c1, 5, 0, 500, -1), 0.05);
-  y2 = setInterval(()=>movc(c2, 10, 10, 500, 500), 100);
-  y3 = setInterval(()=>movc(c3, 0, 10, -1, 500), 100);
-}
-
-function stop() {
-  clearInterval(y1);
-  clearInterval(y2);
-  clearInterval(y3);
-}
-
-class Xobject {
-  static NUMBER_OF_OBJECTS = 0;
-  constructor(x, y, r, m, vx, vy, color, lifespan=10, parent_element=null) {
-    this.lifespan = lifespan;
-    this.element = Xobject.createXobject(parent_element || document.getElementsByTagName("body"));
-    this.setRadius(r);
-    this.setColor(color);
-    this.setPosition(x, y);
-    this.vx = vx;
-    this.vy = vy;
-    this.m = m;
-  }
-
-  setColor(color, alpha=1) {
-    this.color = color;
-    this.element.style.backgroundColor = color(alpha);
-  }
-  setRadius(radius) {
-    this.r = radius;
-    this.element.style.width = this.r+"px";
-    this.element.style.height = this.r+"px";
-  }
-  setPosition(x, y) {
-    this.x = x;
-    this.y = y;
-    this.element.style.left = this.x+"px";
-    this.element.style.top = this.y+"px";
-  }
-  setStepVector(vx, vy) {
-    this.vx = vx;
-    this.vy = vy;
-  }
-
-  devour(xobj) {
-    return xobj;
-  }
-
-  touches(xobj) {
-    let x1 = this.x, x1r = this.x+this.r, y1 = this.y, y1r = this.y+this.r;
-    let x2 = xobj.x, x2r = xobj.x+xobj.r, y2 = xobj.y, y2r = xobj.y+xobj.r;
-
-    return ((x2>=x1 && x2<=x1r) || (x2r>=x1 && x2r<=x1r)) && ((y2>=y1 && y2<=y1r) || (y2r<=y1 && y2r<=y1r)) ? true: false;
-  }
-
-  static reproduce(xobj_1, xobj_2) {
-    return [xobj_1, xobj_2];
-  }
-  static createXobject(parent_element){
-    let element = document.createElement("div");
-    Xobject.NUMBER_OF_OBJECTS += 1;
-    element.id = `xo_${Xobject.NUMBER_OF_OBJECTS}`;
-    element.classList.add("circle");
-    parent_element.appendChild(element);
-    return element;
-  }
-}
-
-class MashWorld {
-  N = 700;
-  M = 700;
-  R = 5.00; // threshold a new object produces
-  DEFAULT_RADIUS_COMPS = 30;
-  COLORS = [
-    (a) => {
-      return `rgba(100, 0, 0, ${a})`;
-    },
-    (a) => {
-      return `rgba(0, 100, 0, ${a})`;
-    },
-    (a) => {
-      return `rgba(0, 0, 100, ${a})`;
-    }
-  ];
-
-  constructor(N = 700, M = 700, R = 0.5) {
-    this.N = N;
-    this.M = M;
-    this.R = R;
-    this.objects = [];
-  }
-
-  createNewObject(x, y, r, vx, vy) {
-    let xobj = new Xobject(
-      x,
-      y,
-      r,
-      1,
-      vx,
-      vy,
-      this.COLORS[getRndInteger(0, 2)],
-      10,
-      document.getElementById("board")
-    );
-    this.objects.push(xobj);
-    return xobj;
-  }
-
-  spawnNewObject(smin, smax, r=this.DEFAULT_RADIUS_COMPS) {
-    let rv = Math.random();
-    if (rv >= this.R) {
-      let xobj = this.createNewObject(
-        getRndInteger(0, this.N-r),
-        getRndInteger(0, this.M-r),
-        r,
-        getRndInteger(smin, smax),
-        getRndInteger(smin, smax),
-      );
-      return xobj;
-    }
-    return false;
-  }
-
-  run(){
-    this.objects.forEach((e)=>{
-      let cx = parseInt(e.element.style.left);
-      let cy = parseInt(e.element.style.top);
-
-      let fp = calculateFinalPositionAndDirectionInSquareBoundary(cx, cy, e.vx, e.vy, this.N-e.r);
-      
-      
-      e.element.style.left = fp[0]+"px";
-      e.element.style.top = fp[1]+"px";
-      e.vx = fp[2];
-      e.vy = fp[3];
-    });
-    for(let i=0; i<this.objects.length; i++){
-      for(let j=i+1; j<this.objects.length; j++){
-        let temp = this.objects[i].touches(this.objects[j]);
-        if(temp)
-          console.log(`${this.objects[i].element.id} <=> ${this.objects[j].element.id}: ${temp}`);
-      }
-    }
-
-  }
-}
 
 // Calculate the final position of an object inside 
 // a BOUNDARY * BOUNDARY square bundary.
@@ -271,6 +93,195 @@ function  calculateFinalPositionAndDirectionInSquareBoundary(x, y, VX, VY, BOUND
 
 
   return [x+cvx, y+cvy, VX, VY];
+}
+
+/* Board Matrix Size */
+N = 700;
+M = 700;
+let BOARD = document.getElementById("board");
+BOARD.style.width = N+"px";
+BOARD.style.height = M+"px";
+
+class Xobject {
+  static PARENT_ELEMENT = document.getElementById('board');
+  static NUMBER_OF_OBJECTS = 0;
+  constructor(x, y, r, vx, vy, color, lifespan=10) {
+    this.color = color;
+    this.lifespan = lifespan;
+    this.element = Xobject.createXobject(Xobject.PARENT_ELEMENT);
+    this.setRadius(r);
+    this.setColor();
+    this.setPosition(x, y);
+    this.vx = vx;
+    this.vy = vy;
+    this.passed_lifetime = 1;
+  }
+  getLifeForce(){
+    return 1-parseFloat(this.passed_lifetime/this.lifespan).toFixed(2);
+  }
+  setColor() {
+    this.element.style.backgroundColor = this.color(this.getLifeForce());
+  }
+  setRadius(radius) {
+    this.r = radius;
+    this.element.style.width = this.r+"px";
+    this.element.style.height = this.r+"px";
+  }
+  setPosition(x, y) {
+    this.x = x;
+    this.y = y;
+    this.element.style.left = this.x+"px";
+    this.element.style.top = this.y+"px";
+  }
+  setStepVector(vx, vy) {
+    this.vx = vx;
+    this.vy = vy;
+  }
+
+  devour(xobj) {
+    return xobj;
+  }
+
+  touches(xobj) {
+    let x1 = this.x, x1r = this.x+this.r, y1 = this.y, y1r = this.y+this.r;
+    let x2 = xobj.x, x2r = xobj.x+xobj.r, y2 = xobj.y, y2r = xobj.y+xobj.r;
+
+    if(x1r<x2 || x1>x2r)
+      return false;
+    if(y1r<y2 || y1>y2r)
+      return false;
+    return true;
+  }
+
+  destroy() {
+    Xobject.PARENT_ELEMENT.removeChild(this.element);
+  }
+
+  static reproduce(xobj_1, xobj_2) {
+    return [xobj_1, xobj_2];
+  }
+
+  static createXobject(parent_element){
+    let element = document.createElement("div");
+    Xobject.NUMBER_OF_OBJECTS += 1;
+    element.id = `xo_${Xobject.NUMBER_OF_OBJECTS}`;
+    element.classList.add("circle");
+    parent_element.appendChild(element);
+    return element;
+  }
+}
+
+class MashWorld {
+  static N = 700;
+  static M = 700;
+  static R = 5.00; // threshold a new object produces
+  DEFAULT_RADIUS_COMPS = 30;
+  COLORS = [
+    (a) => {
+      return `rgba(100, 0, 0, ${a})`;
+    },
+    (a) => {
+      return `rgba(0, 100, 0, ${a})`;
+    },
+    (a) => {
+      return `rgba(0, 0, 100, ${a})`;
+    }
+  ];
+
+  constructor(
+    object_creation_threshold, 
+    default_lifespan, 
+    smashing_threshold, 
+    minimum_size_range, 
+    maximum_size_range,
+    minimum_speed_range,
+    maximum_speed_range
+  ) {
+    this.N = MashWorld.N;
+    this.M = MashWorld.M;
+    this.R = MashWorld.R;
+    this.object_creation_threshold = object_creation_threshold;
+    this.default_lifespan = default_lifespan;
+    this.smashing_threshold = smashing_threshold;
+    this.minimum_size_range = minimum_size_range;
+    this.maximum_size_range = maximum_size_range;
+    this.minimum_speed_range = minimum_speed_range;
+    this.maximum_speed_range = maximum_speed_range;
+    this.objects = [];
+  }
+  smash(bxo, sxo) {
+    let number_of_objects = parseInt(bxo.r / sxo.r);
+    for(let t=0; t<number_of_objects; t++) {
+      this.createNewObject(
+        bxo.x,
+        bxo.y,
+        sxo.r,
+        [bxo.vx, -bxo.vx][getRndInteger(0, 1)],
+        [bxo.vy, -bxo.vy][getRndInteger(0, 1)],
+        bxo.color,
+        bxo.lifespan+sxo.lifespan
+      );
+    }
+  }
+  createNewObject(x, y, r, vx, vy) {
+    let xobj = new Xobject(
+      x,
+      y,
+      r,
+      vx,
+      vy,
+      this.COLORS[getRndInteger(0, 2)],
+      10
+    );
+    this.objects.push(xobj);
+    return xobj;
+  }
+
+  spawnNewObject(smin, smax, r=this.DEFAULT_RADIUS_COMPS) {
+    let rv = Math.random();
+    if (rv >= this.R) {
+      let xobj = this.createNewObject(
+        getRndInteger(0, this.N-r),
+        getRndInteger(0, this.M-r),
+        r,
+        getRndInteger(smin, smax),
+        getRndInteger(smin, smax),
+      );
+      return xobj;
+    }
+    return false;
+  }
+
+  run(oct=0.5, smin=-7, smax=7, r=this.DEFAULT_RADIUS_COMPS){
+    this.spawnNewObject(smin, smax, r);
+    this.objects.forEach((e)=>{
+      let cx = parseInt(e.element.style.left);
+      let cy = parseInt(e.element.style.top);
+
+      let fp = calculateFinalPositionAndDirectionInSquareBoundary(cx, cy, e.vx, e.vy, this.N-e.r);
+      
+      
+      e.element.style.left = fp[0]+"px";
+      e.element.style.top = fp[1]+"px";
+      e.vx = fp[2];
+      e.vy = fp[3];
+      e.passed_lifetime += 1;
+      e.setColor();
+      if(e.passed_lifetime>=e.lifespan) {
+        this.objects.splice(this.objects.indexOf(e), 1);
+        e.destroy();
+      }
+    });
+    for(let i=0; i<this.objects.length; i++){
+      for(let j=i+1; j<this.objects.length; j++){
+        let temp = this.objects[i].touches(this.objects[j]);
+        if(temp)
+          console.log(`${this.objects[i].element.id} <=> ${this.objects[j].element.id}: ${temp}`);
+          if(this.objects[])
+      }
+    }
+
+  }
 }
 
 
